18deff1fd0e0b3b1bfa1dfe8bcbc0bc266d54c20
Philipp Schuster <phip1611@gmail.com>
2021-01-21 11:28:19 +0100

    bin: can either use pwd or dir passed by first arg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::io::stdin;
 use nflz::{get_renamable_files, can_rename_all, NFLZError};
 use std::process::exit;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let pwd = std::env::current_dir().unwrap();
-    let rn_map = get_renamable_files(&pwd).unwrap();
-    if let Err(e) = can_rename_all(&pwd, &rn_map) {
+    let dir = get_dir();
+
+    if !dir.is_dir() {
+        eprint!("NFLZ: {:?} is not a directory.", dir);
+        exit(-1);
+    } else {
+        eprintln!("NFLZ: using dir: {:?}", dir);
+    }
+
+    let rn_map = get_renamable_files(dir.as_path()).unwrap();
+    if let Err(e) = can_rename_all(dir.as_path(), &rn_map) {
         eprintln!("Can't rename the following files");
         match e {
             NFLZError::ConflictingFiles(files) => {
@@ -16,6 +25,16 @@ fn main() {
         exit(-1)
     } else {
 
+    }
+}
+
+/// Returns either PWD or the dir specified by first argument as [`PathBuf`].
+fn get_dir() -> PathBuf {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() > 1 {
+        Path::new(&args[1]).to_path_buf()
+    } else {
+        std::env::current_dir().unwrap()
     }
 }
 
