591e69e16ab65b499fef9efb7c1ae73d998eef73
Philipp Schuster <phip1611@gmail.com>
2021-01-21 21:40:35 +0100

    Ready for Release (0.1.0)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
-//! Library to handle "numbered (ascending) filenames with leading zeroes"
+/*
+MIT License
+
+Copyright (c) 2021 Philipp Schuster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//! Library to handle "numbered (ascending) filenames with leading zeros"
 //! It provides the functionality to fetch all files in a given directory,
 //! that match the pattern `<prefix>(<number>)<suffix)`, like `paris (101).jpg`.
 //! The library can provide you the following functionality:
--- src/nflz.rs
+++ src/nflz.rs
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) 2021 Philipp Schuster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 //! Module related to renaming files.
 
 use std::collections::{BTreeMap, HashSet};
@@ -27,7 +50,7 @@ pub fn compute_rename_map(pf_list: &Vec<ParsedFilename>) -> RenameMap {
     for pf in pf_list {
         let digits = digits(pf.number_group_value());
         let add_digits_count = max_digits - digits;
-        let value_str_with_leading_zeroes = format!(
+        let value_str_with_leading_zeros = format!(
             "{}{}",
             String::from("0").repeat(add_digits_count as usize),
             pf.number_group_value()
@@ -35,7 +58,7 @@ pub fn compute_rename_map(pf_list: &Vec<ParsedFilename>) -> RenameMap {
         let new_filename = format!(
             "{}{}{}",
             pf.filename_prefix(),
-            value_str_with_leading_zeroes,
+            value_str_with_leading_zeros,
             pf.filename_suffix(),
         );
 
